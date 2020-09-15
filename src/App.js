a8cf5837bb502a2fe08aff9e2f992df654b32197
Shalini DC <shalini.1325@gmail.com>
2020-09-14 18:31:29 -0700

    setting up stripe

--- src/App.js
+++ src/App.js
@@ -11,55 +11,57 @@ import {useStateValue} from "./context/StateProvider";
 import { loadStripe } from "@stripe/stripe-js";
 import {Elements} from "@stripe/react-stripe-js";
 
+const promise = loadStripe('pk_test_51HPvTeAIlxIWcrxuDl9s7KtBEDNOoVZm3S6RkaFmkU6MMOj8FbWAOwAukBlBXyD5ar2v9opjsZWuUUpEhjyRMqin00iomd7AV4');
+
 function App() {
 
-    const[{}, dispatch] = useStateValue();
+  const[{}, dispatch] = useStateValue();
 
-    useEffect(()=>{
+  useEffect(()=>{
 
-        auth.onAuthStateChanged(authUser => {
-            console.log('The user is', authUser);
+    auth.onAuthStateChanged(authUser => {
+      console.log('The user is', authUser);
 
-            if(authUser){
-                dispatch({
-                    type: 'SET_USER',
-                    user: authUser
-                })
-            } else {
-                dispatch({
-                    type: 'SET_USER',
-                    user: null
-                })
-            }
+      if(authUser){
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
         })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
 
-    }, []);
+  }, []);
 
-    return (
-        <Router>
-            <div className="App">
-                    <Switch>
-                        <Route path='/login'>
-                            <Login />
-                        </Route>
-                        <Route path="/checkout">
-                            <Header />
-                            <Checkout />
-                        </Route>
-                        <Route path='/payment'>
-                            <Header />
-                            <Elements stripe={promise}>
-                                <Payment />
-                            </Elements>
-                        </Route>
-                        <Route path='/'>
-                            <Header />
-                            <Home />
-                        </Route>
-                    </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path='/payment'>
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
